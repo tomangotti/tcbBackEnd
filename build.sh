1738c79b27f875a33f10bbb1ff8b81f0e551a244
thomas <tom.angotti11@gmail.com>
2023-11-02 11:48:56 -0600

    udpated build.sh file

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 # exit on error
 set -o errexit
 
-python -m pip install poetry==1.1.4
+# Ensure poetry uses the correct Python interpreter
+/opt/render/project/src/.venv/bin/python -m poetry env use /opt/render/project/src/.venv/bin/python
 
-poetry install
+# Install project dependencies using Poetry
+/opt/render/project/src/.venv/bin/python -m poetry install
 
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Collect static files and perform database migrations
+/opt/render/project/src/.venv/bin/python manage.py collectstatic --no-input
+/opt/render/project/src/.venv/bin/python manage.py migrate
