46dc4ca8f50933cfb32caa74334b0c98e9d5ce89
thomas <tom.angotti11@gmail.com>
2023-11-02 11:52:07 -0600

    test build file

--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@
 # exit on error
 set -o errexit
 
-# Ensure poetry uses the correct Python interpreter
-/opt/render/project/src/.venv/bin/python -m poetry env use /opt/render/project/src/.venv/bin/python
+poetry install
 
-# Install project dependencies using Poetry
-/opt/render/project/src/.venv/bin/python -m poetry install
 
-# Collect static files and perform database migrations
-/opt/render/project/src/.venv/bin/python manage.py collectstatic --no-input
-/opt/render/project/src/.venv/bin/python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
