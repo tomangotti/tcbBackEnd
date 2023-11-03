3db10b32466d86f2cff011567b8416401e4c20de
thomas <tom.angotti11@gmail.com>
2023-11-03 13:21:47 -0600

    testing new image file dist

--- recipes/urls.py
+++ recipes/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path("share/recipe/<str:code>", ShareRecipeWithUser.as_view())
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
