cb65dbd2a0d58b14ca25e06855138de7694c94ad
thomas <tom.angotti11@gmail.com>
2023-11-07 10:42:42 -0700

    testing static file location

--- tcbBackEnd/settings.py
+++ tcbBackEnd/settings.py
@@ -133,11 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "media/")
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     
 
