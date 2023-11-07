02a98c62a2a2a8f42783534ccb94c0edda376022
thomas <tom.angotti11@gmail.com>
2023-11-07 10:14:27 -0700

    testing whitenoise

--- tcbBackEnd/settings.py
+++ tcbBackEnd/settings.py
@@ -135,9 +135,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
 
 
--- tcbBackEnd/urls.py
+++ tcbBackEnd/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('recipes/', include("recipes.urls"))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
