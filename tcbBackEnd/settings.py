cb32453e165b5e076175ddfc281e365270ed451c
thomas <tom.angotti11@gmail.com>
2023-11-07 09:38:56 -0700

    added if not debug line for storage

--- tcbBackEnd/settings.py
+++ tcbBackEnd/settings.py
@@ -136,7 +136,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# testing 
+if not DEBUG:
+    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# testing
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
