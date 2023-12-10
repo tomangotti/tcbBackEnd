26edc02244d00fb85df71805e1396204e66290de
thomas <tom.angotti11@gmail.com>
2023-12-10 16:02:17 -0700

    updated requirements.txt file

--- tcbBackEnd/settings.py
+++ tcbBackEnd/settings.py
@@ -94,8 +94,11 @@ DATABASES = {
 }
 
 
-database_url = os.environ.get('DATABASE_URL')
-DATABASES["default"] = dj_database_url.parse(database_url)
+# database_url = os.environ.get('DATABASE_URL')
+# # DATABASES["default"] = dj_database_url.parse(database_url)
+# DATABASES = {
+#     'default': dj_database_url.parse(database_url, conn_max_age=600)
+# }
 
 
 # Password validation
