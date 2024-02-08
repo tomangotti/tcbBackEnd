9cc4ea7445d0bf9475086ef84ebed72eb854051d
thomas <tom.angotti11@gmail.com>
2024-02-08 10:34:38 -0700

    second attempt

--- recipeCollections/serializer.py
+++ recipeCollections/serializer.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 
 from recipes.models import Recipes
+from recipes.serializers import RecipesSerializer
 from django.contrib.auth.models import User
 from .models import Collections
 
 
 
 class CollectionSerializer(serializers.ModelSerializer):
-    recipes = serializers.PrimaryKeyRelatedField(many=True, queryset=Recipes.objects.all())
+    recipes = RecipesSerializer(many=True, read_only=True)
 
     class Meta:
         model = Collections
