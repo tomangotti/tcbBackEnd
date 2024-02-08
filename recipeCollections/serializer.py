b97c364b909155bf97d137973fb7e84b5ce6a982
thomas <tom.angotti11@gmail.com>
2024-02-08 10:27:42 -0700

    added functionality for single collection to return recipe data

--- recipeCollections/serializer.py
+++ recipeCollections/serializer.py
@@ -4,7 +4,11 @@ from recipes.models import Recipes
 from django.contrib.auth.models import User
 from .models import Collections
 
+
+
 class CollectionSerializer(serializers.ModelSerializer):
+    recipes = serializers.PrimaryKeyRelatedField(many=True, queryset=Recipes.objects.all())
+
     class Meta:
         model = Collections
         fields = ('id', 'name', 'description', 'user', 'recipes',)
