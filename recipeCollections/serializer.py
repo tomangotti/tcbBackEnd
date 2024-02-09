59427dc45223435ea3f6fdb54773274017661dfa
thomas <tom.angotti11@gmail.com>
2024-02-09 10:52:09 -0700

    modifed the postnewcollection

--- recipeCollections/serializer.py
+++ recipeCollections/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 
 from recipes.models import Recipes
 from recipes.serializers import RecipesSerializer
@@ -7,9 +8,22 @@ from .models import Collections
 
 
 class CollectionSerializer(serializers.ModelSerializer):
-    recipes = RecipesSerializer(many=True)
+    recipes = serializers.ListField(
+        child=serializers.PrimaryKeyRelatedField(queryset=Recipes.objects.all()),
+        write_only=True
+    )
     user_username = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = Collections
         fields = ('id', 'name', 'description', 'user', 'recipes', 'user_username')
+
+    def create(self, validated_data):
+        recipes_data = validated_data.pop('recipes', [])
+        collection = Collections.objects.create(**validated_data)
+
+        for recipe_id in recipes_data:
+            recipe = get_object_or_404(Recipes, pk=recipe_id.pk)
+            collection.recipes.add(recipe)
+
+        return collection
