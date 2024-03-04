954c6faf80053193521c2cf6ca2887da6a071038
thomas <tom.angotti11@gmail.com>
2024-03-04 12:22:49 -0700

    forgot url

--- recipeCollections/urls.py
+++ recipeCollections/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import GetUsersCollectionRating, UpdateCollection ,GetUsersCollections, PostNewCollection, AddRecipeToCollection, RemoveRecipeFromCollection, DeleteCollection, GetSingleCollection
+from .views import AddNewRatingView, GetUsersCollectionRating, UpdateCollection ,GetUsersCollections, PostNewCollection, AddRecipeToCollection, RemoveRecipeFromCollection, DeleteCollection, GetSingleCollection
 
 
 urlpatterns = [
@@ -14,7 +14,7 @@ urlpatterns = [
     path("get-single/<int:collection_id>", GetSingleCollection.as_view()),
     path("update/<int:collection_id>", UpdateCollection.as_view()),
     path("<str:collection_id>/ratings/<str:user_id>", GetUsersCollectionRating.as_view()),
-
+    path("ratings/new", AddNewRatingView.as_view()),
 ]
 
 
