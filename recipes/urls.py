f4d8ab03b8302c901d7e64db93a18fa73bf9504e
thomas <tom.angotti11@gmail.com>
2024-04-22 16:43:20 -0600

    twaeking path

--- recipes/urls.py
+++ recipes/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('users/<str:code>', GetUsersRecipes.as_view()),
     path('<str:recipe_id>/ratings/<str:user_id>',GetUsersRecipeRating.as_view()),
     path('feed/recipes/<str:user_id>', GetFeedRecipes.as_view()),
-    path('feed/recipes/slim', GetSlimFeedRecipes.as_view())
+    path('feed/slim/recipes', GetSlimFeedRecipes.as_view())
     
 ]
 
