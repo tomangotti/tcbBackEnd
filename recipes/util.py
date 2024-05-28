7ba4f4e2c4744752269fbb8b63a3e2abaef057eb
thomas <tom.angotti11@gmail.com>
2024-05-28 11:06:33 -0600

    updated code to be more effienct for main request

---
+++ recipes/util.py
@@ -0,0 +1,35 @@
+from .serializers import RecipeSerializer
+from recipeCollections.serializer import CollectionSerializer
+from ususers.serializers import QuickGlanceSerializer
+from ususers.models import User, ProfileImage
+
+
+
+def transform_recipe_data(recipe_list, request):
+    serializer = RecipeSerializer(recipe_list, many=True)
+    serializer_data = serializer.data
+
+    for recipe_data in serializer_data:
+        if recipe_data['image']:
+            recipe_data['image'] = request.build_absolute_uri(recipe_data['image'])
+
+    return serializer_data
+
+
+
+def transform_collection_data(collection):
+    serializer = CollectionSerializer(collection, many=True)
+    serializer_data = serializer.data
+
+    return serializer_data
+
+
+def transform_user_data(user, request):
+    serializer = QuickGlanceSerializer(user, many=True)
+    serializer_data = serializer.data
+
+    for user_data in serializer_data:
+        if user_data['profile_image']:
+            user_data['profile_image'] = request.build_absolute_uri(user_data['profile_image'])
+
+    return serializer_data
