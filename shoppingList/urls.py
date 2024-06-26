fb3ab22247973efe50ce5cf2c8c5a54f1dac7d46
thomas <tom.angotti11@gmail.com>
2024-06-26 15:29:23 -0600

    missed 1 url

--- shoppingList/urls.py
+++ shoppingList/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # import shoppingList.views as views
-from .views import DeleteShoppingListItem, ChangeItemStatus, CreateNewShoppingList, GetUsersShoppingLists, AddNewItemToShoppingList, AddRecipeIngredientsToShopingList
+from .views import GetShoppingListDetails, DeleteShoppingListItem, ChangeItemStatus, CreateNewShoppingList, GetUsersShoppingLists, AddNewItemToShoppingList, AddRecipeIngredientsToShopingList
 
 urlpatterns = [
     path('create/new/shopping_list', CreateNewShoppingList.as_view()),
@@ -11,5 +11,7 @@ urlpatterns = [
     path('add/item/to/shopping_list/<int:list_id>', AddNewItemToShoppingList.as_view()),
     path('add/recipe_ingredients/shopping_list/<int:list_id>/<int:recipe_id>', AddRecipeIngredientsToShopingList.as_view()),
     path('delete/item/<int:item_id>', DeleteShoppingListItem.as_view()),
-    path('change/item/status/<int:item_id>', ChangeItemStatus.as_view())
+    path('change/item/status/<int:item_id>', ChangeItemStatus.as_view()),
+    path('get/details/<int:list_id>', GetShoppingListDetails.as_view()),
+
 ]
